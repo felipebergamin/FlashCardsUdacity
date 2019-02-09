0ac4d081e79d47b98baae63c0c9a759be1d99181
Felipe Bergamin <felipebergamin6@gmail.com>
2019-02-09 11:44:49 -0200

    wrap App inside a Provider

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import AddDeck from './components/AddDeck';
+import reducers from './reducers';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1 }}>
+          <AddDeck />
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
