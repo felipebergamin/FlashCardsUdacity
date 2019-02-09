ff8c05a4db251605f62a5f646b98a0a720180df5
Felipe Bergamin <felipebergamin6@gmail.com>
2019-02-09 15:47:06 -0200

    feat: add StackNavigator

--- App.js
+++ App.js
@@ -2,17 +2,36 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
 import AddDeck from './components/AddDeck';
 import reducers from './reducers';
 import middlewares from './middlewares';
+import DeckList from './components/DeckList';
+
+const StackNavigator = createAppContainer(
+  createStackNavigator({
+    Home: {
+      screen: DeckList,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    AddDeck: {
+      screen: AddDeck,
+      navigationOptions: {
+        header: null,
+      },
+    },
+  })
+);
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers, middlewares)}>
         <View style={{ flex: 1 }}>
-          <AddDeck />
+          <StackNavigator />
         </View>
       </Provider>
     );
